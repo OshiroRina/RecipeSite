e736b947702b5dade78ca99b5d0f76ae4c10f3cb
OshiroRina <rina0428dayo@gmail.com>
2023-06-02 18:05:49 +0900

    スライダー修正等

--- resources/js/Components/Slider.vue
+++ resources/js/Components/Slider.vue
@@ -1,36 +1,39 @@
 <script setup>
 import { Link, router } from '@inertiajs/vue3';
 
-const slides = [{ src: '/storage/images/recipe1/oyakodon.jpg',link:'/recipeDetail/1' },
-{ src: '/storage/images/recipe2/chanchan.jpg' ,link:'/recipeDetail/2'},
-{ src: '/storage/images/recipe6/pepetama.jpg' ,link:'/recipeDetail/6'},
-{ src: '/storage/images/recipe7/misoshiru.jpg' ,link:'/recipeDetail/7'},
+const slides = [{ src: '/storage/images/recipe1/oyakodon.jpg', link: '/recipeDetail/1' },
+{ src: '/storage/images/recipe2/chanchan.jpg', link: '/recipeDetail/2' },
+{ src: '/storage/images/recipe6/pepetama.jpg', link: '/recipeDetail/6' },
+{ src: '/storage/images/recipe7/misoshiru.jpg', link: '/recipeDetail/7' },
 ];
 
 const colors = ['grey-lighten-3'];
+
+const isSmartPhone = () => {
+  // UserAgentからのスマホ判定
+  if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 </script>
 <template>
-  <div class="h-68 md:h-fit">
-    <!-- <v-sheet
-          :color="colors[0]"
-          height="100%"
-          class="w-full mx-auto mt-10"
-        > -->
-    <v-carousel cycle hide-delimiter-background show-arrows="hover" class="h-auto md:h-screen lg:h-screen">
+  <div>
+    <v-carousel cycle hide-delimiter-background show-arrows="hover" height="900" v-if="!isSmartPhone()">
       <Link v-for="(slide, i) in slides" :key="i" :href="slide.link">
-
-      <v-carousel-item :key="i" :src="slide.src" class="hover:opacity-75 cursor-pointer">
-
-        <!-- <Link :href="slide.link" class="hover:opacity-75 w-full">
-        <div class="flex justify-center align-center">
-          <img :src="slide.src">
-        </div>
-      </Link> -->
-
-      </v-carousel-item>
-    </Link>
-
+        <v-carousel-item :key="i" :src="slide.src" class="hover:opacity-75 cursor-pointer">
+        </v-carousel-item>
+      </Link>
     </v-carousel>
-    <!-- </v-sheet> -->
+
+    <!-- スマホ表示用 -->
+    <v-carousel cycle hide-delimiter-background show-arrows="hover" height="300" v-if="isSmartPhone()">
+      <Link v-for="(slide, i) in slides" :key="i" :href="slide.link">
+        <v-carousel-item :key="i" :src="slide.src" class="hover:opacity-75 cursor-pointer">
+        </v-carousel-item>
+      </Link>
+    </v-carousel>
+
   </div>
 </template>
--- resources/js/Pages/User/RecipeSearch.vue
+++ resources/js/Pages/User/RecipeSearch.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { router, Head, Link ,useForm } from '@inertiajs/vue3';
-import { reactive, ref ,computed} from "vue";
+import { router, Head, Link, useForm } from '@inertiajs/vue3';
+import { reactive, ref, computed } from "vue";
 import { useStore } from "vuex";
 import UserLayout from "@/Layouts/UserLayout.vue";
 import Favorite from "@/Components/Favorite.vue";
@@ -11,14 +11,14 @@ const props = defineProps({
     primary_categories: Array
 });
 
-const checkbox =ref(false);
+const checkbox = ref(false);
 
 const store = useStore();
 
 const form = useForm({
     primary_category: store.state.searchRecipes.primary_category,
     secondary_category: store.state.searchRecipes.secondary_category,
-    search_word:store.state.searchRecipes.search_word,
+    search_word: store.state.searchRecipes.search_word,
 });
 
 // 検索時ローディングの実装
@@ -38,36 +38,31 @@ const searchRecipes = async () => {
 
 </script>
 <style>
-#input-10{
-    padding: 0;
-}
-#input-13{
-    padding: 0;
+.input {
+    vertical-align:middle;
 }
 </style>
 <template>
     <UserLayout>
+
         <Head title="Search" />
         <div class="py-12 my-20">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <!-- 詳細検索フォーム -->
-                <v-checkbox
-                    v-model="checkbox"
-                    :label="'Buscar con detalle'"
-                ></v-checkbox>
+                <v-checkbox v-model="checkbox" :label="'Buscar con detalle (詳細検索)'"></v-checkbox>
                 <v-card class="mb-5 py-5 px-5" v-show="checkbox">
                 <div class="text-gray-500 font-bold mb-3">Buscar con mas detalle (詳細検索)</div>
                 <div>
                 <v-form>
-                <v-row no-gutters>
-                    <v-col cols="12" md="5" class="mr-5">
+                <v-row no-gutters class="mb-3">
+                    <v-col cols="12" md="5" class="mr-5 mb-2">
                         <v-select label="Primera Categoria" v-model="form.primary_category" :items="props.primary_categories"
-                                        item-title="name" item-value="id" density="compact" return-object clearable>
+                                 dense hide-details item-title="name" item-value="id" density="compact" return-object clearable>
                         </v-select>
                     </v-col>
                     <v-col cols="12" md="5">
                         <v-select label="Segunda Categoria" v-model="form.secondary_category" :items="form.primary_category.secondary_categories"
-                                        item-title="name" item-value="id" density="compact" return-object clearable v-if="form.primary_category != null"></v-select>
+                        dense hide-details item-title="name" item-value="id" density="compact" return-object clearable v-if="form.primary_category != null"></v-select>
                     </v-col>
                 </v-row>
                 <v-row no-gutters>
@@ -86,14 +81,16 @@ const searchRecipes = async () => {
                     <div class="mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                         <div class="text-2xl font-bold tracking-tight text-gray-900">Recetas (レシピ)</div>
                         <!-- 検索結果無しの場合 -->
-                        <div v-if="props.categories[0] == null" class="pt-10 text-lg text-gray-500">No hay resultados para tu búsqueda.</div>
+                        <div v-if="props.categories[0] == null" class="pt-10 text-lg text-gray-500">No hay resultados para
+                            tu búsqueda. (検索結果が見つかりませんでした)</div>
                         <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                             <div class="group relative" v-for="recipe in props.categories" :key="recipe.id">
                                 <div
                                     class="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                     <Link :href="route('user.recipe.show', { id: recipe.id })">
                                     <img :src="'/storage/images/NoImage.png'"
-                                        class="h-full w-full object-cover object-center lg:h-full lg:w-full" v-if="recipe.image == null">
+                                        class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                        v-if="recipe.image == null">
                                     <img :src="'/storage' + recipe.image"
                                         class="h-full w-full object-cover object-center lg:h-full lg:w-full" v-else>
                                     </Link>
@@ -101,26 +98,25 @@ const searchRecipes = async () => {
                                 <div class="mt-4 flex justify-between">
                                     <div>
                                         <h3 class="text-sm text-gray-700">
-                                            <Link :href="route('user.recipe.show', { id: recipe.id })" class="hover:opacity-75">
-                                                <span aria-hidden="true" class="absolute inset-0"></span>
-                                                {{ recipe.name }}
+                                            <Link :href="route('user.recipe.show', { id: recipe.id })"
+                                                class="hover:opacity-75">
+                                            <span aria-hidden="true" class="absolute inset-0"></span>
+                                            {{ recipe.name }}
                                             </Link>
                                         </h3>
                                         <p class="mt-1 text-sm text-gray-500">{{ recipe.primary_category.name }}</p>
                                     </div>
                                     <p class="text-sm font-medium">
-                                        <Favorite :recipe="recipe" :user_id="user_id"/>
+                                        <Favorite :recipe="recipe" :user_id="user_id" />
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Link
-                    :href="route('user.topSite.index')"
-                    class="bg-gray-500 text-white px-10 py-2 rounded hover:opacity-75 mr-2"
-                    cols="12">
-                    volver
+                <Link :href="route('user.topSite.index')"
+                    class="bg-gray-500 text-white px-10 py-2 rounded hover:opacity-75 mr-2" cols="12">
+                volver
                 </Link>
             </div>
         </div>
