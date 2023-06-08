9aa0dc6e43296e4e0d7e32aeca00cf53b554a12a
OshiroRina <rina0428dayo@gmail.com>
2023-06-08 15:09:54 +0900

    ログイン、登録画面の色変更

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -18,7 +18,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="border-gray-300 focus:border-gray-400 focus:ring-gray-400 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
